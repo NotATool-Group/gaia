9caa3b6b701b3eea874b0c69779ad4c51f6ac2f5
Michele Dalle Rive <dallem@usi.ch>
2024-02-06 12:18:00 +0100

    Backend: Add black and pre-commit.

--- gaia/settings.py
+++ gaia/settings.py
@@ -39,21 +39,42 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost", "gaia.local"]
 
 # Application definition
 
-INSTALLED_APPS = ["django.contrib.admin", "django.contrib.auth", "django.contrib.contenttypes",
-                  "django.contrib.sessions", "django.contrib.messages", "django.contrib.staticfiles", ]
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
 
-MIDDLEWARE = ["django.middleware.security.SecurityMiddleware", "django.contrib.sessions.middleware.SessionMiddleware",
-              "django.middleware.common.CommonMiddleware", "django.middleware.csrf.CsrfViewMiddleware",
-              "django.contrib.auth.middleware.AuthenticationMiddleware",
-              "django.contrib.messages.middleware.MessageMiddleware",
-              "django.middleware.clickjacking.XFrameOptionsMiddleware", ]
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
 
 ROOT_URLCONF = "gaia.urls"
 
-TEMPLATES = [{"BACKEND": "django.template.backends.django.DjangoTemplates", "DIRS": [], "APP_DIRS": True, "OPTIONS": {
-    "context_processors": ["django.template.context_processors.debug", "django.template.context_processors.request",
-                           "django.contrib.auth.context_processors.auth",
-                           "django.contrib.messages.context_processors.messages", ], }, }, ]
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = "gaia.wsgi.application"
 
@@ -73,10 +94,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [{"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", },
-                            {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
-                            {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
-                            {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", }, ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
