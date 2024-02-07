adb53814dd71ea6e6f032ccd75116541ad2c1afc
Michele Dalle Rive <dallem@usi.ch>
2024-02-07 11:17:05 +0100

    Frontend: Install and setup Vuetify.
    `vite-plugin-vuetify` was installed as well: this allows to avoid useless components import, making the builds lightweight and faster.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,22 @@
-import { resolve } from "path";
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import {resolve} from "path";
+import {fileURLToPath, URL} from "url";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    outDir: resolve("./dist"),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve("./src/main.js"),
-      },
+    build: {
+        manifest: true, outDir: resolve("./dist"), emptyOutDir: true, rollupOptions: {
+            input: {
+                main: resolve("./src/main.js"),
+            },
+        },
+    }, base: "/static/", server: {
+        origin: "http://localhost:5173",
+    }, plugins: [vue(), vuetify({autoImport: true})], resolve: {
+        alias: [{
+            find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)),
+        },],
     },
-  },
-  base: "/static/",
-  server: {
-    origin: "http://localhost:5173",
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    ],
-  },
 });
