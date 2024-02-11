c219a54bb54ab3c54556336c21e01e870bb55854
Michele Dalle Rive <dallem@usi.ch>
2024-02-11 12:33:47 +0100

    Frontend: Add authentication guard and automatic login check.
    Before navigating to any page, whether the user is logged in is checked. If the user is logged in the store, everything's good. Otherwise, tries to log in with the backend.
    Then, depending on whether the user is logged in or not, the navigation redirects to login or continues to the protected page.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "@/router/routes.js";
+import store from "@/store";
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to) => {
+  const requiresAuth = to.meta?.requiresAuth ?? false;
+  await store.dispatch("auth/checkAuth");
+
+  if (requiresAuth && !store.getters["auth/isAuthenticated"]) {
+    return "/login";
+  }
+
+  return true;
 });
 
 export default router;
