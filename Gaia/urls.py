4d338e1c72af77880cb3f0a52aae21a755723706
Michele Dalle Rive <dallem@usi.ch>
2024-02-18 16:57:53 +0100

    Backend: Add integration tests for frontend+backend interaction.
    Add tests to make sure that the backend and frontend work together both in development and in production.
    When ENVIRONMENT="dev", Django will serve the running vite dev server, in order to allow hot reload.
    In production, the frontend must be built (`npm run build`), statics must be collected (`python3 manage.py collectstatic`) and then Django will serve the built frontend.
    These tests make sure that all of that works smoothly even when changes are introduced.

--- Gaia/urls.py
+++ Gaia/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("GaiaAuth.urls")),
     # render index if nothing matches
-    re_path(r"^.*$", index),
+    re_path(r"^.*$", index, name="index"),
 ]
